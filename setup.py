ad18c9c528af3394746555cbcae52ed9dc96a84c
Nitesh Agrahari <niteshjnt@gmail.com>
2024-10-21 00:23:00 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Nitesh",
+    author_email="niteshjnt@gmail.com",
+    packages=find_packages()
+)
